5dcaf02ece58bc94f86739a499faad3f45ecc4b3
bartek <bartoszbud@gmail.com>
2024-03-30 21:43:04 +0100

    app init

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,7 +11,7 @@ import {
 import { LockOutlined } from "@mui/icons-material";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+//import axios from "axios";
 
 export const Register = () => {
     const [name, setName] = useState("");
